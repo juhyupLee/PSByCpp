aa384853b77c3376da60e0b0bcf0730e8027d32e
leeJuhyup <juhyup12@naver.com>
2024-03-12 22:47:55 +0900

    [Bronze V] Title: 오늘 날짜, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.cc
@@ -0,0 +1,57 @@
+// PSByCpp.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main()
+{
+
+	//freopen("input.txt", "r", stdin);
+	//int n;
+	//vector<Bridge> inputVec;
+
+	//cin >> n;
+	//
+	//for(int i=0 ;i<n;++i)
+	//{
+	//	int left, right = 0;
+
+	//	cin >> left >> right;
+
+	//	inputVec.push_back({ left,right });
+	//}
+
+	//
+
+	//for(auto node : inputVec)
+	//{
+
+	//	
+	//	int result = 1;
+	//	int divide = 1;
+	//	for(int i= node.right ; i>node.right-node.left; --i)
+	//	{
+	//		result *= i;
+	//		result /= divide++;
+	//		
+	//	}
+	//	cout << result << endl;
+
+
+
+	//	//Outer(node.left, node.right);
+	//	//cout << result << endl;
+	//	//result = 0;
+
+	//}
+
+	cout << "2024-03-12";
+
+}
